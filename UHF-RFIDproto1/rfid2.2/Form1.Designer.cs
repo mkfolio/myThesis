6324fbc39ddb46b3a7847c3f3175085aa49ebcae
mkfolio <32631485+mkfolio@users.noreply.github.com>
2017-11-07 23:47:10 +0800

    Up
    Uploading

---
+++ UHF-RFIDproto1/rfid2.2/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace rfid2._2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.AutoOpenCOM = new System.Windows.Forms.Button();
+            this.ClosePort = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.ParameterTab = new System.Windows.Forms.TabPage();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.QueryTab = new System.Windows.Forms.TabPage();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.ListView1_EPC = new System.Windows.Forms.ListView();
+            this.listViewCol_ID = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.listViewCol_Times = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.QueryTag = new System.Windows.Forms.Button();
+            this.Timer_Test_ = new System.Windows.Forms.Timer(this.components);
+            this.tabControl1.SuspendLayout();
+            this.ParameterTab.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.QueryTab.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // AutoOpenCOM
+            // 
+            this.AutoOpenCOM.Location = new System.Drawing.Point(6, 19);
+            this.AutoOpenCOM.Name = "AutoOpenCOM";
+            this.AutoOpenCOM.Size = new System.Drawing.Size(75, 23);
+            this.AutoOpenCOM.TabIndex = 0;
+            this.AutoOpenCOM.Text = "AutoOpen";
+            this.AutoOpenCOM.UseVisualStyleBackColor = true;
+            this.AutoOpenCOM.Click += new System.EventHandler(this.AutoOpenCOM_Click);
+            // 
+            // ClosePort
+            // 
+            this.ClosePort.Location = new System.Drawing.Point(6, 48);
+            this.ClosePort.Name = "ClosePort";
+            this.ClosePort.Size = new System.Drawing.Size(75, 23);
+            this.ClosePort.TabIndex = 1;
+            this.ClosePort.Text = "Close Port";
+            this.ClosePort.UseVisualStyleBackColor = true;
+            this.ClosePort.Click += new System.EventHandler(this.ClosePort_Click);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.ParameterTab);
+            this.tabControl1.Controls.Add(this.QueryTab);
+            this.tabControl1.Location = new System.Drawing.Point(-2, -2);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(347, 313);
+            this.tabControl1.TabIndex = 2;
+            // 
+            // ParameterTab
+            // 
+            this.ParameterTab.Controls.Add(this.groupBox1);
+            this.ParameterTab.Location = new System.Drawing.Point(4, 22);
+            this.ParameterTab.Name = "ParameterTab";
+            this.ParameterTab.Padding = new System.Windows.Forms.Padding(3);
+            this.ParameterTab.Size = new System.Drawing.Size(339, 287);
+            this.ParameterTab.TabIndex = 0;
+            this.ParameterTab.Text = "Parameter";
+            this.ParameterTab.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.AutoOpenCOM);
+            this.groupBox1.Controls.Add(this.ClosePort);
+            this.groupBox1.Location = new System.Drawing.Point(6, 7);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(144, 203);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Communication";
+            // 
+            // QueryTab
+            // 
+            this.QueryTab.Controls.Add(this.groupBox2);
+            this.QueryTab.Controls.Add(this.QueryTag);
+            this.QueryTab.Location = new System.Drawing.Point(4, 22);
+            this.QueryTab.Name = "QueryTab";
+            this.QueryTab.Padding = new System.Windows.Forms.Padding(3);
+            this.QueryTab.Size = new System.Drawing.Size(339, 287);
+            this.QueryTab.TabIndex = 1;
+            this.QueryTab.Text = "Query";
+            this.QueryTab.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.ListView1_EPC);
+            this.groupBox2.Location = new System.Drawing.Point(10, 48);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(317, 189);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "groupBox2";
+            // 
+            // ListView1_EPC
+            // 
+            this.ListView1_EPC.AccessibleRole = System.Windows.Forms.AccessibleRole.IpAddress;
+            this.ListView1_EPC.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ListView1_EPC.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.listViewCol_ID,
+            this.listViewCol_Times});
+            this.ListView1_EPC.FullRowSelect = true;
+            this.ListView1_EPC.GridLines = true;
+            this.ListView1_EPC.Location = new System.Drawing.Point(6, 19);
+            this.ListView1_EPC.Name = "ListView1_EPC";
+            this.ListView1_EPC.Size = new System.Drawing.Size(305, 164);
+            this.ListView1_EPC.TabIndex = 1;
+            this.ListView1_EPC.UseCompatibleStateImageBehavior = false;
+            this.ListView1_EPC.View = System.Windows.Forms.View.Details;
+            // 
+            // listViewCol_ID
+            // 
+            this.listViewCol_ID.Text = "ID";
+            this.listViewCol_ID.Width = 249;
+            // 
+            // listViewCol_Times
+            // 
+            this.listViewCol_Times.Text = "Times";
+            // 
+            // QueryTag
+            // 
+            this.QueryTag.Location = new System.Drawing.Point(246, 19);
+            this.QueryTag.Name = "QueryTag";
+            this.QueryTag.Size = new System.Drawing.Size(75, 23);
+            this.QueryTag.TabIndex = 0;
+            this.QueryTag.Text = "Query Tag";
+            this.QueryTag.UseVisualStyleBackColor = true;
+            this.QueryTag.Click += new System.EventHandler(this.QueryTag_Click);
+            // 
+            // Timer_Test_
+            // 
+            this.Timer_Test_.Tick += new System.EventHandler(this.Timer_Test__Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(344, 308);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "UHF RFID";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.ParameterTab.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.QueryTab.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button AutoOpenCOM;
+        private System.Windows.Forms.Button ClosePort;
+        private System.Windows.Forms.TabPage QueryTab;
+        private System.Windows.Forms.Button QueryTag;
+        private System.Windows.Forms.TabPage ParameterTab;
+        private System.Windows.Forms.GroupBox groupBox1;
+        protected internal System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ListView ListView1_EPC;
+        private System.Windows.Forms.ColumnHeader listViewCol_ID;
+        private System.Windows.Forms.ColumnHeader listViewCol_Times;
+        private System.Windows.Forms.Timer Timer_Test_;
+    }
+}
+
---
+++ UHF-RFIDproto1/rfid2.2/Form1.cs
@@ -0,0 +1,249 @@
+﻿using System;
+using System.Text;
+using System.Windows.Forms;
+using ReaderB;
+
+namespace rfid2._2
+{
+    public partial class Form1 : Form
+    {
+        private bool fAppClosed;
+        private int port;
+        private int OpenedPort;
+        private byte ComAddr = 0xFF;
+        private byte Baud;
+        private int frmcomportindex;
+        private int fOpenComIndex;
+        private bool ComOpen = false;
+        private int fCmdRet = 30;
+        private bool fIsInventoryScan;
+        private string fInventory_EPC_List;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private string ByteArrayToHexString(byte[] data)
+        {
+            StringBuilder sb = new StringBuilder(data.Length * 3);
+            foreach (byte b in data)
+                sb.Append(Convert.ToString(b, 16).PadLeft(2, '0'));
+            return sb.ToString().ToUpper();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            fIsInventoryScan = false;
+            QueryTab.Enabled = false;
+        }
+
+        private void AutoOpenCOM_Click(object sender, EventArgs e)
+        {
+            port = 0;
+            int openresult;
+
+            openresult = 30;
+
+            Cursor = Cursors.WaitCursor;
+
+            try
+            {
+                ComAddr = Convert.ToByte("FF", 16);
+                Baud = Convert.ToByte(5);
+                openresult = StaticClassReaderB.AutoOpenComPort(ref port, ref ComAddr, Baud, ref frmcomportindex);
+                fOpenComIndex = frmcomportindex;
+                if(openresult == 0)
+                {
+                    ComOpen = true;
+                    OpenedPort = port;
+
+                    if ((fCmdRet == 0x35) | (fCmdRet == 0x30))
+                    {
+                        MessageBox.Show("Serial Communication Error or Occupied", "Error" ,MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        StaticClassReaderB.CloseSpecComPort(frmcomportindex);
+                        ComOpen = false;
+                    }
+                }
+            }
+            finally
+            {
+                Cursor = Cursors.Default;
+            }
+
+            if ((fOpenComIndex != -1 ) & (openresult != 0X35 ) & (openresult != 0X30 ))
+            {
+                MessageBox.Show("Com Port Activated");
+                QueryTab.Enabled = true;
+            }
+
+            if ((fOpenComIndex == -1) && (openresult == 0x30))
+            {
+                MessageBox.Show("Serial Communication Error", "Information", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+               
+
+        }
+
+        private void QueryTag_Click(object sender, EventArgs e)
+        {
+            Timer_Test_.Enabled = !Timer_Test_.Enabled;
+            if (!Timer_Test_.Enabled)
+            {
+                QueryTag.Text = "Query Tag";
+            }
+            else
+            {
+                ListView1_EPC.Items.Clear();
+                QueryTag.Text = "Stop";
+            }
+        }
+
+        public void ChangeSubItem(ListViewItem ListItem, int subItemIndex, string ItemText)
+        {
+            if (subItemIndex == 1)
+            {
+                if (ItemText == "")
+                {
+                    ListItem.SubItems[subItemIndex].Text = ItemText;
+                    if (ListItem.SubItems[subItemIndex + 2].Text == "")
+                    {
+                        ListItem.SubItems[subItemIndex + 2].Text = "1";
+                    }
+                    else
+                    {
+                        ListItem.SubItems[subItemIndex + 2].Text = Convert.ToString(Convert.ToInt32(ListItem.SubItems[subItemIndex + 2].Text) + 1);
+                    }
+                }
+
+                else
+                if (ListItem.SubItems[subItemIndex].Text != ItemText)
+                {
+                    ListItem.SubItems[subItemIndex].Text = ItemText;
+                    ListItem.SubItems[subItemIndex + 2].Text = "1";
+                }
+
+                else
+                {
+                    ListItem.SubItems[subItemIndex + 2].Text = Convert.ToString(Convert.ToInt32(ListItem.SubItems[subItemIndex + 2].Text) + 1);
+                    if ((Convert.ToUInt32(ListItem.SubItems[subItemIndex + 2].Text) > 9999))
+                        ListItem.SubItems[subItemIndex + 2].Text = "1";
+                }
+
+            }
+
+            if (subItemIndex == 2)
+            {
+                if (ListItem.SubItems[subItemIndex].Text != ItemText)
+                {
+                    ListItem.SubItems[subItemIndex].Text = ItemText;
+                }
+            }
+
+        }
+
+        private void Timer_Test__Tick(object sender, EventArgs e)
+        {
+            if (fIsInventoryScan)
+                return;
+            Inventory();
+        }
+
+        private void Inventory()
+        {
+            int i;
+            int CardNum = 0;
+            int Totallen = 0;
+            int EPClen, m;
+            byte[] EPC = new byte[5000];
+            int CardIndex;
+            string temps;
+            string s, sEPC;
+            bool isonlistview;
+            fIsInventoryScan = true;
+            byte AdrTID = 0;
+            byte LenTID = 0;
+            byte TIDFlag = 0;
+
+            ListViewItem aListItem = new ListViewItem();
+            fCmdRet = StaticClassReaderB.Inventory_G2(ref ComAddr, AdrTID, LenTID, TIDFlag, EPC, ref Totallen, ref CardNum, frmcomportindex);
+            //Start Read
+            if((fCmdRet == 1) | (fCmdRet == 2) | (fCmdRet == 3) | (fCmdRet == 4) | (fCmdRet == 0xFB))
+            {
+                byte[] daw = new byte[Totallen];
+                Array.Copy(EPC, daw, Totallen);
+                temps = ByteArrayToHexString(daw);
+                fInventory_EPC_List = temps;
+                m = 0;
+
+                if (CardNum == 0)
+                {
+                    fIsInventoryScan = false;
+                    return;
+                }
+                for (CardIndex = 0;CardIndex<CardNum;CardIndex++)
+                {
+                    EPClen = daw[m];
+                    sEPC = temps.Substring(m * 2 + 2, EPClen * 2);
+                    m = m + EPClen + 1;
+
+                    if (sEPC.Length != EPClen * 2)
+                    return;
+                    
+                    isonlistview = false;
+                    for (i = 0; i < ListView1_EPC.Items.Count; i++)
+                    {
+                        if (sEPC == ListView1_EPC.Items[i].SubItems[1].Text)
+                        {
+                            aListItem = ListView1_EPC.Items[i];
+                            ChangeSubItem(aListItem, 1, sEPC);
+                            isonlistview = true;
+                        }
+                    }
+
+                    if (!isonlistview)
+                    {
+                        aListItem = ListView1_EPC.Items.Add((ListView1_EPC.Items.Count + 1).ToString());
+                        aListItem.SubItems.Add("");
+                        aListItem.SubItems.Add("");
+                        aListItem.SubItems.Add("");
+                        s = sEPC;
+                        ChangeSubItem(aListItem, 1, s);
+                        s = (sEPC.Length / 2).ToString().PadLeft(2, '0');
+                        ChangeSubItem(aListItem, 2, s);
+                    }
+                }
+            }
+            fIsInventoryScan = false;
+            if (fAppClosed)
+                Close();
+
+        }
+
+        private void ClosePort_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                Cursor = Cursors.WaitCursor;
+               
+                fCmdRet = StaticClassReaderB.CloseSpecComPort(OpenedPort);
+                if (fCmdRet == 0)
+                {
+                    ComOpen = false;
+                    QueryTab.Enabled = false;
+                    MessageBox.Show("Com Port Closed", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    MessageBox.Show("Serial Communication Error", "Information", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            finally
+            {
+                Cursor = Cursors.Default;
+            }
+        }
+    }
+}
